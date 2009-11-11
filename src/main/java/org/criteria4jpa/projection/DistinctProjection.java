d378622941fbd3a8cc2adb08607b98a3cf5a75ae
Christian Kaltepoth <christian@kaltepoth.de>
2009-11-11 16:03:00 +0100

    Fixedd distinct projection. Expression must not be parenthesized.

--- src/main/java/org/criteria4jpa/projection/DistinctProjection.java
+++ src/main/java/org/criteria4jpa/projection/DistinctProjection.java
@@ -1,5 +1,8 @@
 package org.criteria4jpa.projection;
 
+import org.criteria4jpa.Criteria;
+import org.criteria4jpa.impl.CriteriaQueryBuilder;
+
 
 /**
  * 
@@ -12,15 +15,27 @@ package org.criteria4jpa.projection;
  * @author Christian Kaltepoth
  *
  */
-public class DistinctProjection extends FunctionProjection {
+public class DistinctProjection implements Projection {
 
+  private final Projection projection;
+  
   /**
    * creates a new "distinct" projection for a wrapped projection.
    * 
    * @param projection wrapped projection
    */
   public DistinctProjection(Projection projection) {
-    super("DISTINCT", projection);
+    this.projection = projection;
   }
-  
+
+  /*
+   * @see org.criteria4jpa.projection.Projection#toQueryString(org.criteria4jpa.Criteria, org.criteria4jpa.impl.CriteriaQueryBuilder)
+   */
+  public String toQueryString(Criteria criteria, CriteriaQueryBuilder queryBuilder) {
+    return new StringBuilder()
+      .append("DISTINCT ")
+      .append( projection.toQueryString(criteria, queryBuilder) )
+      .toString();
+  }
+
 }
